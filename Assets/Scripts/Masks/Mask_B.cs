41e72b3a94cf8aa9ba0c18f55274bc95efc7a3aa
Julian Cortes <juliancortes729@gmail.com>
2026-01-28 20:14:40 -0300

    ADD Respawn de mascaras

---
+++ Assets/Scripts/Masks/Mask_B.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mask_B : Mask
+{
+
+    protected override void Use(GameObject player)
+    {
+        Debug.Log("Blue Mask Used!");
+    }
+}
---
+++ Assets/Scripts/Masks/Mask_R.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mask_R : Mask
+{
+   
+
+    protected override void Use(GameObject player)
+    {
+        Debug.Log("Red Mask Used!");
+    }
+
+}
